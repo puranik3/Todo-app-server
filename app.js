b1837083226348ca83eef05e5aa411c975f69922
Prashanth Puranik <puranik@digdeeper.in>
2024-11-20 15:38:30 +0530

    fixed cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var cors = require('cors');
 
 var app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
